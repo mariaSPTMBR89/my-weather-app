9d12e90a814e69e41713935dd7ac6ca82fa95fc3
mariaSPTMBR89 <maria.mueller.september89@googlemail.com>
2023-03-17 15:49:58 +0100

    JS Update Weatherdata

--- src/app.js
+++ src/app.js
@@ -24,31 +24,54 @@ function formatTime(time) {
 let time = document.querySelector("#time");
 time.innerHTML = formatTime(new Date());
 
-function displayCity(event) {
+function searchCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector(".search input");
-  let city = document.querySelector("h1");
-  city.innerHTML = searchInput.value;
 
   let apiKey = "f3009e4852fa0a079dab291dabf020c4";
   let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 
-  function showTemperature(response) {
+  function showWeather(response) {
+    console.log(response.data);
     let temperature = Math.round(response.data.main.temp);
     let temperatureElement = document.querySelector(".current-temperature");
     temperatureElement.innerHTML = `${temperature}°`;
 
+    let cityName = response.data.name;
+    let city = document.querySelector("h1");
+    city.innerHTML = cityName;
+
     let minTemperature = Math.round(response.data.main.temp_min);
-    let minTemperatureElement = document.querySelector(".range minimum");
+    let minTemperatureElement = document.querySelector("#minimum");
     minTemperatureElement.innerHTML = `${minTemperature}°`;
+
+    let maxTemperature = Math.round(response.data.main.temp_max);
+    let maxTemperatureElement = document.querySelector("#maximum");
+    maxTemperatureElement.innerHTML = `${maxTemperature}°`;
+
+    let description = response.data.weather[0].description;
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = description;
+
+    let windSpeed = Math.round(response.data.wind.speed);
+    let windSpeed00 = document.querySelector("#windSpeed00");
+    windSpeed00.innerHTML = `${windSpeed}km/h`;
+    let windSpeed06 = document.querySelector("#windSpeed06");
+    windSpeed06.innerHTML = `${windSpeed * 2}km/h`;
+    let windSpeed12 = document.querySelector("#windSpeed12");
+    windSpeed12.innerHTML = `${windSpeed / 2}km/h`;
+    let windSpeed18 = document.querySelector("#windSpeed18");
+    windSpeed18.innerHTML = `${windSpeed}km/h`;
+    let windSpeed24 = document.querySelector("#windSpeed24");
+    windSpeed24.innerHTML = `${windSpeed * 2}km/h`;
   }
 
   axios
     .get(`${apiUrl}${searchInput.value}&units=metric&appid=${apiKey}`)
-    .then(showTemperature);
+    .then(showWeather);
 }
 let searchForm = document.querySelector(".search");
-searchForm.addEventListener("submit", displayCity);
+searchForm.addEventListener("submit", searchCity);
 
 function showCurrentTemperature(response) {
   let temperatureLocation = Math.round(response.data.main.temp);
